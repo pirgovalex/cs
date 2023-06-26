87cc3972d09fe965f9cb1d84f31063dc6e9fd359
pirgovalex <alex.pirgatory@gmail.com>
2023-06-26 22:42:01 +0300

    A
    enter the commit message for your changes. Lines starting
    cls
    dotnet "C:\Users\Alex
    HP\AppData\Roaming\Code\User\cs-script.user\dotnet\cscs.dll"  "c:\Python
    and Code\cpp\csharp\pishacoudc#.cs"git push origin my-branch

--- pishacoudc#.cs
+++ pishacoudc#.cs
@@ -2,21 +2,57 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace csharp
-{
-    public class pishacoudc
+
+
+    public class Customer{
+        public string Name {get;set;}
+        public int Counter{get;set;}
+        public int Room{get;set;}
+        public float Price{get;set;}
+        public DateTime expectedENTER{get;set;}
+        public DateTime expectedEXIT{get;set;}
+        public Customer Customer_Parse(string input)
+    {
+        string[] values = input.Split(' ');
+
+        Customer customer = new Customer();
+        customer.Name = values[0];
+        customer.Counter = int.Parse(values[1]);
+        customer.Room = int.Parse(values[2]);
+        customer.Price = float.Parse(values[3]);
+        customer.expectedENTER = DateTime.Parse(values[4]);
+        customer.expectedEXIT = DateTime.Parse(values[5]);
+
+        return customer;
+    }
+
+
+    }
+    public class Program
     {
        static void Main(string[] args)
         {
-            int[,] pache={{1,2,3}
-                         ,{4,5,6}
-                         ,{7,8,9}};
-          int diagonalSum = Enumerable.Range(0, pache.GetLength(0))
-            .Sum(i => pache[i, i]);
-            Console.WriteLine(diagonalSum);
+            int n =2;
+            List<Customer> customers=new List<Customer>();
+
+            for(int i=0;i<=2;i++)
+            {
+                Console.Write("Please input Name,Number of people,room number,price,Check in and Check out\nPlease separate with ' ' ");
+                Customer customer=new Customer();
+                var input=Console.ReadLine();
+                customer = customer.Customer_Parse(input);
+                
+
+                customers.Add(customer);
+            }
+            foreach (Customer c in customers)
+            {
+                Console.WriteLine($"name: {c.Name} how many people: {c.Counter} which room: {c.Room} Price: {c.Price} When they punched in: {c.expectedENTER} and out: {c.expectedEXIT}");
+
+            }
          }
         } 
-    }
+
     
 
     
